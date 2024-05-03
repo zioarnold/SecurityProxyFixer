19447d2add9e0c9ea42fd5132f1c9a131f1d4bec
ARNOL'D CHARYYEV <vl80tk@gmail.com>
2024-05-03 18:53:33 +0200

    Delete constants directory

--- constants/Constants.java
+++
@@ -1,6 +0,0 @@
-package r2u.tools.constants;
-
-public class Constants {
-    public final static String dateTimeFormat = "HH:mm:ss";
-    public final static String SECURITY_PROXY = "security_proxy";
-}
