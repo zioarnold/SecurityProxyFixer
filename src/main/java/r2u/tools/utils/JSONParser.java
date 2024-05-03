747a20790431a9adfc121f9a080f1230851b5041
ARNOL'D CHARYYEV <vl80tk@gmail.com>
2024-05-03 18:13:27 +0200

    Update JSONParser.java

--- src/main/java/r2u/tools/utils/JSONParser.java
+++ src/main/java/r2u/tools/utils/JSONParser.java
@@ -14,7 +14,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class JSONParser {
     private final static Logger logger = Logger.getLogger(JSONParser.class.getName());
@@ -32,21 +31,21 @@ public class JSONParser {
                 query = jsonObject.getString("query");
 
         //Converto in arraylist di stringhe e poi in un hashmap rispettivamente String|String o String|Boolean
-        ArrayList<String> documentList = objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
+        ArrayList<String> documentList = Converters.objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
                 .getJSONObject(0)
                 .getJSONArray("Document").toList());
-        HashMap<String, Boolean> documentMap = convertArrayList2HashMap(documentList);
+        HashMap<String, Boolean> documentMap = Converters.convertArrayList2HashMap(documentList);
 
-        ArrayList<String> netCoList = objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
+        ArrayList<String> netCoList = Converters.objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
                 .getJSONObject(0)
                 .getJSONArray("NetCo").toList());
-        HashMap<String, String> netCo = netCoConverter(netCoList);
+        HashMap<String, String> netCo = Converters.netCoConverter(netCoList);
 
-        ArrayList<String> servCo = objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
+        ArrayList<String> servCo = Converters.objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
                 .getJSONObject(0)
                 .getJSONArray("ServCo").toList());
 
-        ArrayList<String> documentClassList = objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
+        ArrayList<String> documentClassList = Converters.objectClassDocumentConverter(jsonObject.getJSONArray("objectClasses")
                 .getJSONObject(0)
                 .getJSONArray("docClassList").toList());
 
@@ -110,30 +109,5 @@ public class JSONParser {
 
     private static JSONObject getJSON(URL url) throws IOException {
         return new JSONObject(IOUtils.toString(url, StandardCharsets.UTF_8));
-    }
-
-    private static HashMap<String, String> netCoConverter(ArrayList<String> netCo) {
-        HashMap<String, String> netCoMap = new HashMap<>();
-        for (String net : netCo) {
-            netCoMap.put(net.split("=")[0], net.split("=")[1]);
-        }
-        return netCoMap;
-    }
-
-    private static HashMap<String, Boolean> convertArrayList2HashMap(ArrayList<String> documentList) {
-        HashMap<String, Boolean> documentMap = new HashMap<>();
-        for (String document : documentList) {
-            documentMap.put(document.split("=")[0], Boolean.valueOf(document.split("=")[1]));
-        }
-        return documentMap;
-    }
-
-    private static ArrayList<String> objectClassDocumentConverter(List<Object> secProx) {
-        ArrayList<String> securityProxies = new ArrayList<>();
-        //Converto oggetti in stringhe
-        for (Object secProxy : secProx) {
-            securityProxies.add(secProxy.toString());
-        }
-        return securityProxies;
     }
 }
